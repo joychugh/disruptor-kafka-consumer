040e800f535e33c957137542a7eb29d1dfe313da
Joy Chugh <joychugh@gmail.com>
2015-10-18 23:55:28 -0400

    Using Kafka 0.9 Consumer for Disruptor Style Consumer.

---
+++ src/main/java/com/joychugh/disruptor/DisruptorEventFactory.java
@@ -0,0 +1,13 @@
+package com.joychugh.disruptor;
+import com.lmax.disruptor.EventFactory;
+
+/**
+ * @author jchugh
+ */
+public class DisruptorEventFactory<K, V> implements EventFactory<DisruptorEvent<K, V>> {
+
+    @Override
+    public DisruptorEvent<K, V> newInstance() {
+        return new DisruptorEvent<>();
+    }
+}
